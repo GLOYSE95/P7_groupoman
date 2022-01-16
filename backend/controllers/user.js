ef9d1067e0737b8932c4fee18bebc6c7e362855e
Gaelle Loyse <gaelleloyse@yahoo.fr>
2022-01-16 18:32:58 +0100

    cryptage Mdp

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,51 +2,49 @@ const db = require("../models");
 const user = db.users;
 const Op = db.Sequelize.Op;
 const jwt = require("jsonwebtoken"); //genere un token
+const bcrypt = require("bcrypt"); //cryptage du mot de passe
 
 const dotenv = require("dotenv");
 const result = dotenv.config();
 const models = require("../models");
-const { USER } = require("../configuration/dbconfig");
+// const { USER } = require("../configuration/dbconfig");
 
 exports.signup = (req, res, next) => {
-  user
-    .create({
-      prenom: req.body.prenom,
-      nom: req.body.nom,
-      password: req.body.password,
-      email: req.body.email,
-      descript: req.body.descript,
-    })
-    .then((user) => {
-      res.send({ message: "Utilisateur enregistré!" });
-    })
-    .catch((err) => {
-      res.status(500).send({ message: err.message });
-    });
+  bcrypt.hash(req.body.password, 10).then((hash) => {
+    user
+      .create({
+        prenom: req.body.prenom,
+        nom: req.body.nom,
+        password: hash,
+        email: req.body.email,
+        descript: req.body.descript,
+      })
+      .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+      .catch((err) => {
+        res.status(500).send({ message: err.message });
+      });
+  });
 };
 
 exports.login = (req, res) => {
   user
     .findOne({ where: { email: req.body.email } })
-    .then((mail) => {
-      if (!mail) {
+    .then((user) => {
+      if (!user) {
         return res.status(404).send({ message: "email non trouvé." });
       }
-      user
-        .findOne({ where: { password: req.body.password } })
-        .then((password) => {
-          if (!password) {
-            return res.status(401).send({ message: "mot de passe invalide." });
-          } else {
-            var token = jwt.sign({ id: user.id }, `${process.env.TOKENKEY}`, {
-              expiresIn: "24h",
-            });
-            res.status(200).json({
-              id: user.id,
-              acessToken: token,
-            });
-          }
+      var token = jwt.sign({ id: user.id }, `${process.env.TOKENKEY}`, {
+        expiresIn: "24h",
+      });
+      bcrypt.compare(req.body.password, user.password).then((valid) => {
+        if (!valid) {
+          return res.status(401).json({ error: "Mot de passe incorrect !" });
+        }
+        res.status(200).json({
+          id: user.id,
+          acessToken: token,
         });
+      });
     })
     .catch((error) => res.status(500).json({ message: "erreur serveur." }));
 };
